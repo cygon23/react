3b7529981675272bb57b1fcf62e736ce895aea1a
Godfrey <rweyemamu088@gmail.com>
2024-08-27 17:03:19 +0300

    first day

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hellow from "./components/hellow";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Hellow />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/hellow.jsx
@@ -0,0 +1,11 @@
+function Hellow() {
+    return (
+      <div>
+        <h1>return hellow from modified</h1>
+        <h2>kyomah</h2>
+      </div>
+    );
+  }
+  
+  export default Hellow;
+  
