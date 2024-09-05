a2c26794092f635a1ab17dd71e3ad231d72f18e6
Godfrey <rweyemamu088@gmail.com>
2024-09-05 16:24:39 +0300

    food-app:food details

--- food-app/food-app/src/App.jsx
+++ food-app/food-app/src/App.jsx
@@ -10,7 +10,7 @@ import FoodDetails from "./components/FoodDetails";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
-  const[foodId,setFoodId] = useState("");
+  const[foodId,setFoodId] = useState("656329");
   return (
     <div className="App">
       <Nav />
