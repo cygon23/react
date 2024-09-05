86c8fbd708bf81af9cf0c17288e8dfa5e7caf1a9
Godfrey <rweyemamu088@gmail.com>
2024-09-05 16:25:11 +0300

    food-app:food details

--- food-app/food-app/src/components/FoodDetails.jsx
+++ food-app/food-app/src/components/FoodDetails.jsx
@@ -1,43 +1,39 @@
 import { useEffect, useState } from "react";
-import styles from "./fooddetails.module.css";
+
 export default function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
-  const API_KEY = "20e4800974694e6b8ca403b0e20c135d"; // API key should be a string
+  const API_KEY = "c346e9e23fca42bb8cea5c2a11ee8c62";
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data);
       setFood(data);
-      setIsLoading(false);
     }
     fetchFood();
   }, [foodId]);
-
   return (
     <div>
       <div>
         <h1>{food.title}</h1>
-        <img src={food.image} alt={food.title} />
-      </div>
-      <span>
-        <strong>{food.readyInMinutes}Minutes</strong>
-      </span>
-      <span>Serves: {food.serving}</span>
-      <span>{food.vegetarian ? "Vegetarian" : "Non-Vegetarian"}</span>
-      <span>{food.vegan ? "Vegan" : " "}</span>
-      <div>
-        $<span>{food.pricePerServing} Per Serving</span>
-      </div>
-      <div>
-        Instructions:
-        {isLoading? <p>Loading....</p>:food.anaysedInstructions[0].steps.map((step) => (
-          <li>{step.step}</li>
-        ))}
+        <img src={food.image} alt="" />
        
+        <div>
+          <span>
+            <strong>{food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>Serves {food.servings}</strong>
+          </span>
+          <span>{food.vegetarian ? "Vegetarian" : "Non vegetarian"}</span>
+
+          <span>{food.vegan ? "Vegan" : ""}</span>
+        </div>
       </div>
+       <div>
+        $<span>{food.pricePerServing}</span>
+       </div>
     </div>
   );
 }
