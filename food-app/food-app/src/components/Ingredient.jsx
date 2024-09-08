96326bc4f3149a5b594fe9e00f2da0e16f68a4f8
Godfrey <rweyemamu088@gmail.com>
2024-09-08 06:46:16 +0300

    food-app completes

--- food-app/food-app/src/components/Ingredient.jsx
+++ food-app/food-app/src/components/Ingredient.jsx
@@ -5,6 +5,7 @@ export default function Ingredient({ item }) {
       <div className={styles.itemContainer}>
         <div className={styles.imageContainer}>
           <img
+           className={styles.image}
             src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`}
             alt=""
           />
